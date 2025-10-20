3d9f0cf7bb7f784a99ad0b5d0cc7ac7e6b08ce49
Mohamed <mhmdabdrabo2019@gmail.com>
2025-10-20 04:33:16 +0300

    CoreEntities

---
+++ GymManagementDAL/Entities/Member.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Entities
+{
+    internal class Member : GymUser
+    {
+        //CreatedAt(in BaseEntity) => JoinDate (later)
+
+        public string? Photo { get; set; }
+    }
+}
