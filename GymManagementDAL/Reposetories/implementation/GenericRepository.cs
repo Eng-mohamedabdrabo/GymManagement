bcc6cc38738615d457e73609a10d1780e22511a3
Mohamed <mhmdabdrabo2019@gmail.com>
2025-10-23 20:59:24 +0300

    implement Generic Repository

---
+++ GymManagementDAL/Reposetories/implementation/GenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using GymManagementDAL.Entities;
+using GymManagementDAL.Reposetories.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Reposetories.implementation
+{
+    internal class GenericRepository<TEntity> : IGenericRepositoryInterface
+        where TEntity : BaseEntity, new()
+    {
+    }
+}
