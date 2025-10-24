b8cbe0bfff920d2e6d4880d0fa30e3b3076c56ec
Mohamed <mhmdabdrabo2019@gmail.com>
2025-10-25 02:20:42 +0300

    PlanService Dn

---
+++ GymManagementBLL/Business-Interfaces/Implementation/PlanService.cs
@@ -0,0 +1,135 @@
+﻿using GymManagementBLL.Business_Interfaces.Interfaces;
+using GymManagementBLL.View_Models.Plan_View_Models;
+using GymManagementDAL.Entities;
+using GymManagementDAL.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementBLL.Business_Interfaces.Implementation
+{
+    internal class PlanService : IPlanService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PlanService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IEnumerable<PlanViewModel> GetAllPlans()
+        {
+            var plans = _unitOfWork.GetRepository<Plans>().GetAll(x => x.IsActive == true);
+
+            if (plans is null || !plans.Any())
+                return [];
+
+            return plans.Select(x => new PlanViewModel
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Description = x.Description,
+                DurationDays = x.DurationDays,
+                Price = x.Price,
+                IsActive = x.IsActive
+            });
+        }
+
+        public PlanViewModel? GetPlanDetails(int planId)
+        {
+            var plan = _unitOfWork.GetRepository<Plans>().GetById(planId);
+
+            if (plan is null || !plan.IsActive || HasActiveMemberShip(planId))
+                return null;
+
+            return new PlanViewModel
+            {
+                Name = plan.Name,
+                Description = plan.Description,
+                DurationDays = plan.DurationDays,
+                Price = plan.Price,
+                IsActive = plan.IsActive
+            };
+        }
+
+
+        public PlanToUpdateViewModel? GetPlanToUpdate(int planId)
+        {
+            var plan = _unitOfWork.GetRepository<Plans>().GetById(planId);
+
+            if (plan is null || !plan.IsActive)
+                return null;
+
+            return new PlanToUpdateViewModel
+            {
+                Name = plan.Name,
+                Description = plan.Description,
+                DurationDays = plan.DurationDays,
+                Price = plan.Price
+            };
+        }
+
+
+        public bool Toggle(int planId)
+        {
+            try
+            {
+                var planRepo = _unitOfWork.GetRepository<Plans>();
+                var plan = planRepo.GetById(planId);
+
+                if (plan is null)
+                    return false;
+
+                (plan.IsActive, plan.UpdatedAt) = (!plan.IsActive, DateTime.Now);
+
+                planRepo.Update(plan);
+                var result = _unitOfWork.SaveChanges();
+
+                return result > 0;
+            }
+            catch (Exception )
+            {
+                return false;
+            }
+        }
+
+
+        public bool UpdatePlan(int planId, PlanToUpdateViewModel planToUpdate)
+        {
+            try
+            {
+                var plan = _unitOfWork.GetRepository<Plans>().GetById(planId);
+
+                if (plan is null || !plan.IsActive)
+                    return false;
+
+                plan.Name = planToUpdate.Name;
+                plan.Description = planToUpdate.Description;
+                plan.DurationDays = planToUpdate.DurationDays;
+                plan.Price = planToUpdate.Price;
+                plan.UpdatedAt = DateTime.Now;
+
+                _unitOfWork.GetRepository<Plans>().Update(plan);
+                var result = _unitOfWork.SaveChanges();
+
+                return result > 0;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+
+        #region Helper
+        bool HasActiveMemberShip(int planId)
+        {
+           var memberShipStatus = _unitOfWork.GetRepository<Memberships>()
+                .GetAll(X => X.PlansId == planId && X.Status == "Active").Any();
+            return memberShipStatus ;
+        }
+        #endregion
+    }
+}
---
+++ GymManagementBLL/Business-Interfaces/Interfaces/IPlanService.cs
@@ -0,0 +1,22 @@
+﻿using GymManagementBLL.View_Models.Plan_View_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementBLL.Business_Interfaces.Interfaces
+{
+    internal interface IPlanService
+    {
+        IEnumerable<PlanViewModel> GetAllPlans();
+
+        PlanViewModel? GetPlanDetails(int PlanId);
+
+        PlanToUpdateViewModel? GetPlanToUpdate(int PlanId);
+
+        bool UpdatePlan(int planId, PlanToUpdateViewModel planToUpdate);
+
+        bool Toggle(int planId);
+    }
+}
